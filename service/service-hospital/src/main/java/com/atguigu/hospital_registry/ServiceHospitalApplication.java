1eb657a9f406f3d65be56ceccc6f3a4156e8ebb5
seashine <hh_nsul@outlook.com>
2022-03-04 14:54:32 +0800

    1. service-hospital module created 2. hospital_registry_hospital database created 3. hospital_set table created

---
+++ service/service-hospital/src/main/java/com/atguigu/hospital_registry/ServiceHospitalApplication.java
@@ -0,0 +1,12 @@
+package com.atguigu.hospital_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceHospitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHospitalApplication.class, args);
+    }
+}
