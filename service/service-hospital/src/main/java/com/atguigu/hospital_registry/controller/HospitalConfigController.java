80f519b35806b170f97375013c36fd79687bac7c
seashine <hh_nsul@outlook.com>
2022-03-08 10:30:58 +0800

    1. Custom Global Exception Handler Class 2. Logback

--- service/service-hospital/src/main/java/com/atguigu/hospital_registry/controller/HospitalConfigController.java
+++ service/service-hospital/src/main/java/com/atguigu/hospital_registry/controller/HospitalConfigController.java
@@ -1,5 +1,6 @@
 package com.atguigu.hospital_registry.controller;
 
+import com.atguigu.hospital_registry.common.exception.HospitalRegistryException;
 import com.atguigu.hospital_registry.common.result.Result;
 import com.atguigu.hospital_registry.common.util.MD5;
 import com.atguigu.hospital_registry.model.hosp.HospitalConfig;
@@ -7,7 +8,6 @@ import com.atguigu.hospital_registry.service.HospitalConfigService;
 import com.atguigu.hospital_registry.vo.hosp.HospitalConfigQueryVo;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import org.apache.ibatis.annotations.Delete;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
